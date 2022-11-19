792693db5caba965053520eba58050450c9ac185
Jerry Setiawan - 535210005 <111833928+JerryS098@users.noreply.github.com>
2022-11-19 20:08:25 +0700

    Delete KRRS directory

--- KRRS/krrs.js
+++
@@ -1,8 +0,0 @@
-function submitprofil() {
-    alert("Data anda berhasil disimpan!");
-
-    var nimmahasiswa = document.getElementById("nim").value;
-    var namamahasiswa = document.getElementById("nama").value;
-    sessionStorage.setItem("nimvalue", nim);
-    sessionStorage.setItem("namavalue", nama);
-}
